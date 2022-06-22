9a95fcf87165d847ad8418c17c07c439256ea2ef
Hanna <venera131916@gmail.com>
2022-06-22 23:05:31 +0300

    feat: add style

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ $(function(){
     $('.design-slider').slick({
 dots: true,
 slidesToShow: 4,
-variableWidth: true
+variableWidth: true,
+prevArrow:'<img class="arrow arrow-left" src="img/arrow-left.svg">',
+nextArrow:'<img class="arrow arrow-right" src="img/arrow-right.svg">',
     });
 });
 
