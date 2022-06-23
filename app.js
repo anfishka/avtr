3dcb545b30807dee5a223e0041d21e6d74b0bba5
Hanna <venera131916@gmail.com>
2022-06-23 10:33:08 +0300

    feat: add style

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ slidesToShow: 4,
 variableWidth: true,
 prevArrow:'<img class="arrow arrow-left" src="img/arrow-left.svg">',
 nextArrow:'<img class="arrow arrow-right" src="img/arrow-right.svg">',
+responsive:[{
+    breakpoint: 361,
+    settings:{
+        variableWith: false,
+        slideToShow: 1,
+    }
+}]
     });
 });
 
